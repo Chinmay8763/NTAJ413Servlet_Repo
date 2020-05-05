a9ca4fe02f0a4a6839fdbd0559a1d88c1275553a
Chinmaya <0chinmayaparamguru@gmail.com>
2020-05-05 07:51:29 +0530

    HttpSessionWithUrlRewritting-SessionTrackingApp4

--- HttpSessionWithUrlRewritting-SessionTracking4/src/com/nt/servlet/FirstServlet.java
+++ HttpSessionWithUrlRewritting-SessionTracking4/src/com/nt/servlet/FirstServlet.java
@@ -28,7 +28,7 @@ public class FirstServlet extends HttpServlet {
 		gender=req.getParameter("gender");
 		//create Session Object and begin the Session
 		ses=req.getSession(true);
-		ses.setMaxInactiveInterval(120);   //Session timeout after 2 mins
+		//ses.setMaxInactiveInterval(120);   //Session timeout after 2 mins
 		//write form1/req1 data to Session obj as session attributes
 		ses.setAttribute("name", name);
 		ses.setAttribute("fname", fname);
@@ -36,7 +36,7 @@ public class FirstServlet extends HttpServlet {
 		
 		//Generate form2 dynamically here
 		pw.println(" <h1 style='color:red;text-align:center'> Provide Income Details </h1>");
-		pw.println(" <form action='secondurl' method='POST'> ");
+		pw.println(" <form action='"+res.encodeURL("secondurl")+"' method='POST'> ");
 		pw.println("<table border='1' bgcolor='cyan' align='center'>");
 		pw.println("<tr><td> Income of this Year </td> <td><input type='text' name='income'> </td></tr>");
 		pw.println("<tr> <td> Tax </td> <td> <input type='text' name='tax'> </td></tr>");
